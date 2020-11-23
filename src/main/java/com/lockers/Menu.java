8653f7f1942e90c251744c4fa4db3f9c2deb39a6
Riyaz J <j.riyazu@gmail.com>
2020-11-23 22:28:01 +0530

    Implement view file in descending order functionality. Write documentation comment at class and method level. Add closeProgram() method in Main program to close the program and show relevant message.

--- src/main/java/com/lockers/Menu.java
+++ src/main/java/com/lockers/Menu.java
@@ -2,6 +2,16 @@ package com.lockers;
 
 import java.util.Scanner;
 
+/**
+ * The Menu program contains few methods along with main method.
+ * It runs the program and perform several operations
+ * like showing command line options to user and verifies
+ * the user input to show relevant result.
+ *
+ * @author Riyaz J
+ * @version 1.0
+ * @since 2020-11-20
+ */
 public class Menu {
 
     //        Scanner object
@@ -12,36 +22,76 @@ public class Menu {
     public static void main(String[] args) {
 
         System.out.println("\n\t\t\t\tWelcome to Lockers. Developed by 'Riyaz J'");
-        System.out.println("\t\t\t\t------------------------------------------\n");
+        System.out.println("\t\t\t\t------------------------------------------");
 
         new Menu().options();
     }
 
+    /**
+     * This method perform several operations like create, delete, search
+     * and show files based on the user input.
+     *
+     * @return nothing.
+     */
     public void options() {
 
-        System.out.println("Select from the option\n");
+        System.out.println("\nSelect from the option\n");
 
-        String options = "1. See All Files\n" +
+        final String options = "1. See All Files\n" +
                 "2. Create File\n" +
                 "3. Delete File\n" +
                 "4. Search File\n" +
                 "5. Close Application";
 
 //        Take int value by the user thorough command line
-        int option = correctOption(options, 5, 1);
+        final int option = correctOption(options, 5, 2);
 
         switch (option) {
 
             case -1:
-                System.err.println("\nYou tried maximum attempts, application is closed. Thank you!");
-                System.exit(0);
+                closeProgram("You tried maximum attempts, application is closed. Thank you!", true);
                 break;
 
 //            See All Files
             case 1:
-                System.out.println("All Files are showed in ascending order by default.\n");
+                System.out.println("\nAll Files are showed in ascending order by default.\n");
                 operation.allFilesAsc()
                         .forEach(System.out::println);
+
+//                Show sub options after the result is shown.
+                final String sub_options = "\n" +
+                        "1. View in Descending Order\n" +
+                        "2. Main Menu\n" +
+                        "3. Close Application";
+
+                final int sub_option = correctOption(sub_options, 3, 2);
+
+                switch (sub_option) {
+
+                    case -1:
+                        closeProgram("You tried maximum attempts, application is closed. Thank you!", true);
+                        break;
+
+                    case 1:
+                        System.out.println("\nAll Files are showed in descending order.\n");
+                        operation.allFilesDsc()
+                                .forEach(System.out::println);
+//                        Call options() method recursively so that program will close only if the user choose to close.
+                        options();
+                        break;
+
+                    case 2:
+//                        recursive call
+                        options();
+                        break;
+
+                    case 3:
+                        closeProgram("Thank you for using our application! Application is closed", false);
+                        break;
+
+                    default:
+                        System.out.println("Nothing has selected");
+                }
                 break;
 
 //            Create File
@@ -61,8 +111,7 @@ public class Menu {
 
 //            Close Application
             case 5:
-                System.out.println("Thank you for using our application! Application is closed");
-                System.exit(0);
+                closeProgram("Thank you for using our application! Application is closed", false);
                 break;
 
             default:
@@ -71,7 +120,22 @@ public class Menu {
         }
     }
 
-    public int correctOption(String options, int lastOption, int maxAttempts) {
+    /**
+     * This method verifies the user input and prompts to choose
+     * correct option and return the user input if the input
+     * meet the requirements.
+     *
+     * @param options     This parameter will be showed to the user
+     *                    to choose a option.
+     * @param lastOption  The user input will be compared with the
+     *                    parameter, if the input is in between the
+     *                    range or not.
+     * @param maxAttempts This parameter is used to limit the
+     *                    chances given to the user to choose
+     *                    correct option.
+     * @return int (It returns the user input value).
+     */
+    public int correctOption(final String options, final int lastOption, final int maxAttempts) {
 
 //        Take int value by user thorough command line
         int option = 0;
@@ -111,5 +175,20 @@ public class Menu {
             loop++;
         }
         return option;
+    }
+
+    /**
+     * This method close the program and show appropriate message.
+     *
+     * @param message This parameter will be showed to the user.
+     * @param error   To show the message as an error or just as info.
+     * @return nothing.
+     */
+    public void closeProgram(final String message, final boolean error) {
+        if (error)
+            System.err.println("\n" + message);
+        else
+            System.out.println("\n" + message);
+        System.exit(0);
     }
 }
--- src/main/java/com/lockers/Operation.java
+++ src/main/java/com/lockers/Operation.java
@@ -1,20 +1,56 @@
 package com.lockers;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * The Operations program contains methods which
+ * performs file operations like
+ * create, delete, retrieve file and etc.
+ *
+ * @author Riyaz J
+ * @version 1.0
+ * @since 2020-11-20
+ */
 public class Operation {
 
-    private final File file = new File("src/main/resources/");
+    private File file = new File("src/main/resources/");
 
-    //    Return List of files in ascending order
+    /**
+     * This method gives the list of all files in ascending order.
+     *
+     * @return List<String> (This returns list of all files).
+     */
     public List<String> allFilesAsc() {
 
         return Arrays.stream(file.list())
                 .sorted()
                 .collect(Collectors.toList());
 
+    }
+
+    /**
+     * This method gives the list of all files in descending order.
+     *
+     * @return List<String> (This returns list of all files).
+     */
+    public List<String> allFilesDsc() {
+
+        List<String> dscList = new ArrayList<>();
+
+        final List<String> ascList = allFilesAsc();
+
+        int i = ascList.size() - 1;
+
+        while (i >= 0) {
+
+            dscList.add(ascList.get(i));
+
+            --i;
+        }
+        return dscList;
     }
 }
